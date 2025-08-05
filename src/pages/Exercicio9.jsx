2838a60ee1a3855a1dcbd8a2ef25ff033d9d5ae5
Lara Pereira <larabernspereira@gmail.com>
2025-08-05 20:20:37 -0300

    add exer 10

--- src/pages/Exercicio9.jsx
+++ src/pages/Exercicio9.jsx
@@ -1,3 +1,19 @@
 export default function Exercicio9() {
-  return <h2>Exercício 9</h2>;
+  let listaTarefas = [
+    "Passear com o cachorro",
+    "Brincar com a vizinha",
+    "Pular corda",
+  ];
+
+  return (
+    <>
+      <h2>Exercício 9</h2>
+
+      <ul>
+        {listaTarefas.map((tarefa, index) => (
+          <li key={index}>{tarefa}</li>
+        ))}
+      </ul>
+    </>
+  );
 }
