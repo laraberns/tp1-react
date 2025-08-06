48c63d637a04454870c7befc44b75fa238c56fc4
Lara Pereira <larabernspereira@gmail.com>
2025-08-05 22:48:32 -0300

    add exer 15 e 16

--- src/pages/Exercicio15.jsx
+++ src/pages/Exercicio15.jsx
@@ -1,3 +1,16 @@
+import { useState } from "react";
+
 export default function Exercicio15() {
-  return <h2>Exercício 15</h2>;
+  const [contador, setContador] = useState(0);
+
+  return (
+    <>
+      <h2>Exercício 15</h2>
+
+      <p>Contador: {contador}</p>
+
+      <button onClick={() => setContador((prev) => prev + 1)}>Aumentar</button>
+      <button onClick={() => setContador((prev) => prev - 1)}>Diminuir</button>
+    </>
+  );
 }
--- src/pages/Exercicio16.jsx
+++ src/pages/Exercicio16.jsx
@@ -1,3 +1,17 @@
+import ExibirProduto from "../components/ExibirProduto";
+
 export default function Exercicio16() {
-  return <h2>Exercício 16</h2>;
+  let produto = { nome: "Mesa", preco: 120, quantidade: 12, custo: 80 };
+  return (
+    <>
+      <h2>Exercício 16</h2>
+
+      <ExibirProduto
+        custo={produto.custo}
+        nome={produto.nome}
+        preco={produto.preco}
+        quantidade={produto.quantidade}
+      />
+    </>
+  );
 }
